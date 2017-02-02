2893837b1df2066f1a388221c7b52996f07e542d
h8324 <h8324@OHJE-330-WS-30>
2017-02-02 10:23:25 +0200

    Initial commit.

---
+++ Olio-esim-person/Olio-esim-person/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Olio_esim_person
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Person person = new Person();
+            //person.firstname = "Pekka";
+
+            //tee yksi oppilasolio
+            Student student = new Student();
+            student.FirstName = "Teppo";
+            student.LastName = "Terävä";
+            student.Address = "Piippukatu 3";
+            student.Age = 23;
+            student.PhoneNumber = "040-84984839";
+            student.StudentID = "h8888";
+            Console.WriteLine(student.ToString());
+
+            Teacher teacher = new Teacher("Kirsi", "Kernel", "D554");
+            teacher.Age = 35;
+            teacher.Address = "Piippukatu 2";
+            teacher.PhoneNumber = "020-1325232";
+                Console.WriteLine(teacher.ToString());
+
+            teacher.TeacherMethod();
+            student.StudentMethod();
+
+            student.PersonMethod(); //<-- Toimiiko? Ei.
+
+            // olio ei ole aliluokan sisällä..
+            //student.PersonMethod(); // <-- toimiiko?
+
+        }
+    }
+}
---
+++ Olio-esim-person/Olio-esim-person/Teacher.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Olio_esim_person
+{
+    class Teacher : Person
+    {
+        public string Room { get; set; }
+        public Teacher()
+        {
+
+        }
+        public Teacher(string firstname, string lastname, string room)
+            : base(firstname, lastname)
+        {
+            Room = room;
+        }
+
+        public void TeacherMethod()
+        {
+            Console.WriteLine("This method belongs to Teacher!");
+        }
+
+        public override string ToString()
+        {
+            //Person-luokan ToString + Room
+            //Firstname + Lastname ... + Room
+            //base viittaa yliluokkaan eli nyt Person-luokkaan
+            return base.ToString() + " " + Room;
+        }
+    }
+}
